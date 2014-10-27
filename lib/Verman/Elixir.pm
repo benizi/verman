94c2d0d6937dc0e53c78a18cc4e3014ce2d5afb0
Benjamin R. Haskell <verman@benizi.com>
2014-10-27 13:51:34 -0400

    Update Elixir for (non-executed) `install` command
    - generates (doesn't execute) the script

--- lib/Verman/Elixir.pm
+++ lib/Verman/Elixir.pm
@@ -2,6 +2,7 @@ package Verman::Elixir;
 use strict;
 use warnings;
 use base 'Verman::SelfContained', 'Verman::Compiled';
+use Verman::Util;
 
 sub new {
   my $self = shift->SUPER::new(@_);
@@ -9,14 +10,34 @@ sub new {
   $self
 }
 
-# To install:
-#   mkdir -p /opt/elixir
-#   git clone https://github.com/elixir-lang/elixir.git /opt/elixir/git
-#   cd /opt/elixir/git
-#   version=v1.0.0
-#   git checkout -f $version
-#   make clean
-#   make compile
-#   make PREFIX=/opt/elixir/versions/$version install
+sub available {
+  my $self = shift;
+  my $root = $self->var($self->_rootvar);
+  $self->_get_source;
+  push_dir path $root, 'git';
+  my @tags = run qw/git tag/;
+  pop_dir;
+  version_sort grep /^v/, @tags;
+}
+
+sub install {
+  my ($self, $version) = @_;
+  $self->_get_source;
+  my $root = $self->var($self->_rootvar);
+  my $versions = $self->var($self->_versvar);
+  my $build = path $root, 'build', $version;
+  my $prefix = path $versions, $version;
+  <<BUILD;
+cd $root/git &&
+mkdir -p $build $versions &&
+printf 'Extracting...' &&
+git archive $version | (cd $build ; tar x) &&
+printf 'Done\\n' &&
+cd $build &&
+make clean &&
+make compile &&
+make PREFIX=$prefix install
+BUILD
+}
 
 1;
