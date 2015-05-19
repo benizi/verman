71f18aa4d8b05f798bb94bc72a0b02fb6049972f
Benjamin R. Haskell <verman@benizi.com>
2015-05-19 01:41:14 -0400

    Make `go install` work like `node install`

--- lib/Verman/Go.pm
+++ lib/Verman/Go.pm
@@ -39,21 +39,21 @@ sub after_path {
 
 sub install {
   my ($self, $version) = @_;
+  $self->_get_source;
   my $root = $self->var($self->_rootvar);
   my $versions = $self->var($self->_versvar);
   my $build = path $root, 'build', $version;
   my $goroot = path $versions, $version;
-  my $url = $self->var($self->_varname('upstream'));
   <<BUILD;
-([[ -d $root/hg ]] || hg clone $url $root/hg) &&
-cd $root/hg &&
-hg archive --rev $version $build &&
+cd $root/git &&
+mkdir -p $build $versions &&
+printf 'Extracting...' &&
+git archive $version | (cd $build ; tar x) &&
+printf 'Done\\n' &&
 cd $build/src &&
 GOROOT_FINAL=$versions/$version sh ./all.bash &&
-mkdir -p $versions &&
-mv $build $versions/$version &&
-mkdir -p $root/path/$version &&
-GOPATH=$root/path/$version $versions/$version/bin/go get code.google.com/p/go.tools/cmd/...
+mv $build $versions &&
+GOPATH=$root/path/$version $goroot/bin/go get golang.org/x/tools/cmd/...
 BUILD
 }
 
