16a15e68555601aca72b5039974040ffc348cc47
Benjamin R. Haskell <verman@benizi.com>
2015-06-11 21:25:19 -0400

    Also install `opam` with OCaml

--- lib/Verman/OCaml.pm
+++ lib/Verman/OCaml.pm
@@ -21,15 +21,33 @@ sub install {
   my $versions = $self->var($self->_versvar);
   my $build = path $root, 'build', $version;
   my $prefix = path $versions, $version;
+
+  my $cache = path $root, 'cache';
+  my $opam_version = '1.2.2';
+  my $opam_base = "opam-full-$opam_version";
+  my $opam_tgz = "$opam_base.tar.gz";
+  my $opam_tar = path $cache, $opam_tgz;
+  my $opam_url = "https://github.com/ocaml/opam/releases/download/$opam_version/$opam_tgz";
+
   <<BUILD;
 cd $root/git &&
-mkdir -p $build $versions &&
+mkdir -p $build $versions $cache &&
 printf 'Extracting...' &&
 git archive $version | (cd $build ; tar x) &&
 printf 'Done\\n' &&
 cd $build &&
 ./configure -prefix $versions/$version &&
 make world.opt &&
+make install &&
+(test -f $opam_tar || curl -L -o $opam_tar $opam_url) &&
+rm -r $build &&
+mkdir -p $build &&
+cd $build &&
+tar --strip-components=1 -zx < $opam_tar &&
+eval \$(VERMAN_EVAL=1 verman ocaml use $version) &&
+./configure --prefix=$prefix &&
+env -u MAKEFLAGS make lib-ext &&
+make &&
 make install
 BUILD
 }
