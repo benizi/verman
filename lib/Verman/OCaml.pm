3d8c7226557cd6df484cb0e9fbcf10ff52c64569
Benjamin R. Haskell <verman@benizi.com>
2015-06-13 22:38:40 -0400

    Build `opam` in separate directory

--- lib/Verman/OCaml.pm
+++ lib/Verman/OCaml.pm
@@ -25,13 +25,14 @@ sub install {
   my $cache = path $root, 'cache';
   my $opam_version = '1.2.2';
   my $opam_base = "opam-full-$opam_version";
+  my $opam_build = path $root, 'build', $opam_base;
   my $opam_tgz = "$opam_base.tar.gz";
   my $opam_tar = path $cache, $opam_tgz;
   my $opam_url = "https://github.com/ocaml/opam/releases/download/$opam_version/$opam_tgz";
 
   <<BUILD;
 cd $root/git &&
-mkdir -p $build $versions $cache &&
+mkdir -p $build $versions $cache $opam_build &&
 printf 'Extracting...' &&
 git archive $version | (cd $build ; tar x) &&
 printf 'Done\\n' &&
@@ -41,9 +42,7 @@ sed -i -e 's%\\(chmod \\)-%\\1a-%' Makefile &&
 make world.opt &&
 make install &&
 (test -f $opam_tar || curl -L -o $opam_tar $opam_url) &&
-rm -r $build &&
-mkdir -p $build &&
-cd $build &&
+cd $opam_build &&
 tar --strip-components=1 -zx < $opam_tar &&
 sed -i -e 's!\\(sed -n -e .\\)s!\\11s!' configure &&
 eval \$(VERMAN_EVAL=1 verman ocaml use $version) &&
