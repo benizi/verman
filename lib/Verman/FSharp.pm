e3939f465891e1d86cdf69f9d296cd42b6e609d5
Benjamin R. Haskell <verman@benizi.com>
2015-09-24 02:11:19 -0400

    Add `fsharp install`

---
+++ lib/Verman/FSharp.pm
@@ -0,0 +1,43 @@
+package Verman::FSharp;
+use strict;
+use warnings;
+use base 'Verman::SelfContained', 'Verman::Compiled';
+use Verman::Util;
+
+sub new {
+  my $self = shift->SUPER::new(@_);
+  $self->var($self->_varname('upstream'), 'https://github.com/fsharp/fsharp');
+  $self
+}
+
+sub available {
+  my $self = shift;
+  my $root = $self->var($self->_rootvar);
+  $self->_get_source;
+  push_dir path $root, 'git';
+  my @tags = run qw/git tag/;
+  pop_dir;
+  version_sort @tags;
+}
+
+sub install {
+  my ($self, $version) = @_;
+  $self->_get_source;
+  my $root = $self->var($self->_rootvar);
+  my $versions = $self->var($self->_versvar);
+  my $build = path $root, 'build', $version;
+  my $prefix = path $versions, $version;
+  <<BUILD;
+cd $root/git &&
+mkdir -p $build $versions &&
+printf 'Extracting...' &&
+git archive $version | (cd $build ; tar x) &&
+printf 'Done\\n' &&
+cd $build &&
+./autogen.sh --prefix="$prefix" &&
+make &&
+make install
+BUILD
+}
+
+1;
