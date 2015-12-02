bf90e1dad2999c6bafff7e02b1291610a863dd2c
Benjamin R. Haskell <verman@benizi.com>
2015-12-02 00:21:41 -0500

    Add `stack install`
    Support for Haskell Stack (only `v0.1.8.0` currently).

---
+++ lib/Verman/Stack.pm
@@ -0,0 +1,32 @@
+package Verman::Stack;
+use strict;
+use warnings;
+use base 'Verman::SelfContained';
+use Verman::Util qw/path/;
+
+sub install {
+  my ($self, $version) = @_;
+  (my $vnum = $version) =~ s/^v//;
+  my $download = "https://github.com/commercialhaskell/stack/releases/download";
+  my $file = "stack-$vnum-linux-x86_64.tar.gz";
+  my $url = "$download/v$vnum/$file";
+  my $root = $self->var($self->_rootvar);
+  my $versions = $self->var($self->_versvar);
+  my $prefix = path $versions, $version;
+  my $cache = path $root, 'downloads';
+  <<BUILD;
+mkdir -p $cache $prefix &&
+printf 'Downloading %s...\\n' "$url" &&
+(test -f $cache/$file || curl -L -o $cache/$file $url) &&
+printf 'Done\\n' &&
+tar --strip-components=1 -xzf $cache/$file -C $prefix &&
+mkdir -p $prefix/bin &&
+mv $prefix/stack $prefix/bin/
+BUILD
+}
+
+sub available {
+  'v0.1.8.0'
+}
+
+1;
