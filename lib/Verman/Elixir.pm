9c843830cb781558872ee8a1a65665722249b3e2
Benjamin R. Haskell <verman@benizi.com>
2016-02-03 23:51:45 -0500

    Build `Elixir` from GitHub tarball

--- lib/Verman/Elixir.pm
+++ lib/Verman/Elixir.pm
@@ -6,32 +6,32 @@ use Verman::Util;
 
 sub new {
   my $self = shift->SUPER::new(@_);
-  $self->var($self->_varname('upstream'), 'https://github.com/elixir-lang/elixir.git');
+  $self->var($self->_varname('upstream'), 'https://github.com/elixir-lang/elixir');
   $self
 }
 
 sub available {
-  my $self = shift;
-  my $root = $self->var($self->_rootvar);
-  $self->_get_source;
-  push_dir path $root, 'git';
-  my @tags = run qw/git tag/;
-  pop_dir;
-  version_sort grep /^v/, @tags;
+  version_sort grep /^v/, shift->SUPER::_tags
 }
 
 sub install {
   my ($self, $version) = @_;
-  $self->_get_source;
   my $root = $self->var($self->_rootvar);
   my $versions = $self->var($self->_versvar);
   my $build = path $root, 'build', $version;
   my $prefix = path $versions, $version;
+  my $github = $self->upstream . "/archive/$version.tar.gz";
   <<BUILD;
-cd $root/git &&
 mkdir -p $build $versions &&
-printf 'Extracting...' &&
-git archive $version | (cd $build ; tar x) &&
+if test -d $root/git
+then
+  printf 'Extracting...' &&
+  cd $root/git &&
+  git archive $version | (cd $build ; tar x)
+else
+  printf 'Downloading...' &&
+  curl -Ls $github | (cd $build ; tar zx --strip-components=1)
+fi &&
 printf 'Done\\n' &&
 cd $build &&
 make clean &&
