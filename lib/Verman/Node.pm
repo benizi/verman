0c442184f31fa5bc33cf87d5f145a16ffded5a49
Benjamin R. Haskell <verman@benizi.com>
2020-05-11 09:29:45 -0400

    Add Nix versions for Node
    Symlink each `nodejs` executable into `$versions/bin` so `$versions/bin`
    is writable (outside `/nix/store`). Set `NPM_CONFIG_PREFIX` so global
    modules aren't installed in user homedir.

--- lib/Verman/Node.pm
+++ lib/Verman/Node.pm
@@ -1,7 +1,7 @@
 package Verman::Node;
 use strict;
 use warnings;
-use base 'Verman::SelfContained', 'Verman::Compiled';
+use base 'Verman::Nix', 'Verman::SelfContained', 'Verman::Compiled';
 use Verman::Util;
 
 sub new {
@@ -10,8 +10,21 @@ sub new {
   $self
 }
 
-sub available {
+sub _non_nix_available {
   version_sort grep /^v/, shift->SUPER::_tags
+}
+
+sub _nix_stub_dirs {
+  my $self = shift;
+  my ($v, $prefix) = @_;
+  path $prefix, 'bin'
+}
+
+sub _nix_symlinks {
+  my $self = shift;
+  my ($v, $prefix, $nix_root) = @_;
+  my $bin = path $prefix, 'bin';
+  map [$bin, "$nix_root/bin/$_"], qw/node npm npx/
 }
 
 sub _make_install {
@@ -27,8 +40,13 @@ sub after_path {
   my ($self) = @_;
   my $versions = $self->var($self->_versvar);
   my $v = $self->var($self->_vervar);
+  my $prefix = path $versions, $v;
   $self->no_pathlike(NODE_PATH => $versions, 1);
-  $self->pre_pathlike(NODE_PATH => path $versions, $v, 'lib', 'node_modules');
+  $self->pre_pathlike(NODE_PATH => path $prefix, 'lib', 'node_modules');
+  $self->env_vars(NPM_CONFIG_PREFIX => $prefix)
 }
+
+sub _nix_version_prefix { 'v' }
+sub _nixpkg { 'nodejs' }
 
 1;
